94c778c2f77ce6fee4de61d0f9f578ce00e53b58
liuliang <liuliang@111.com.cn>
2019-09-02 11:34:16 +0800

    修改配置

--- template/src/main.js
+++ template/src/main.js
@@ -8,9 +8,9 @@ import App from './App'
 import router from './router'
 {{/router}}
 {{#vuex}}  //vuex为true的时候就会写入这些
-import Vuex from 'vuex';{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import store from  './store/index';{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-Vue.use(Vuex){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import Vuex from 'vuex'
+import store from  './store/index'
+Vue.use(Vuex)
 {{/vuex}}
 
 Vue.config.productionTip = false
